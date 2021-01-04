d26ea5a5e1d316e9f9f8122c449560cb2e3bd9ef
Ivan Mota <ivan_mota09@hotmail.com>
2021-01-03 21:23:07 -0600

    Updates

---
+++ precache-manifest.c88acf214760c91d83773b65aa3446f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82359d3e88facd18c62b",
+    "url": "/css/app.bd70d789.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ed83c81e8a2f1f54c9bfbb33541b69f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769f49e3176af21dee69",
+    "url": "/js/about.2848bba9.js"
+  },
+  {
+    "revision": "82359d3e88facd18c62b",
+    "url": "/js/app.a0b51874.js"
+  },
+  {
+    "revision": "9a9112111167a0e63aaf",
+    "url": "/js/chunk-vendors.de53c0a6.js"
+  },
+  {
+    "revision": "cb86264b147ddd03dd286b9090c07e44",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
