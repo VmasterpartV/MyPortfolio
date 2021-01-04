49af63e71fcd417ffa5f8747a48c861536f8cbf3
Ivan Mota <ivan_mota09@hotmail.com>
2021-01-04 04:04:51 -0600

    First Update Home

---
+++ src/components/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar topNav" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <router-link class="navbar-item" tag="a" to="/">
+        <img
+          src="https://bulma.io/images/bulma-logo.png"
+          alt="Bulma: Free, open source, and modern CSS framework based on Flexbox"
+          width="112"
+          height="28"
+        />
+      </router-link>
+
+      <a
+        role="button"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 2;
+  background-color: white;
+  justify-content: space-around;
+  width: 100%;
+  border-bottom: 1px solid lightgray;
+}
+
+.topNav {
+  border-top: 5px solid #206bc2;
+}
+</style>
