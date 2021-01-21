8292dc8bd9a3f54c964d521e5653bb1a6948dbd4
Ivan Mota <ivan_mota09@hotmail.com>
2021-01-21 17:06:54 -0600

    Plantilla

--- vue.config.js
+++ vue.config.js
@@ -1,40 +1,6 @@
-'use strict'
-const defaultSettings = require('./src/settings.js')
-
-const name = defaultSettings.title || 'Bulma Admin Null' // page title
-
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, Mac: sudo npm run
-// You can change the port by the following method:
-// port = 9527 npm run dev OR npm run dev --port = 9527
-const port = process.env.port || process.env.npm_config_port || 8080 // dev port
-
-// All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
-  /**
-   * You will need to set publicPath if you plan to deploy your site under a sub path,
-   * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
-   * then publicPath should be set to "/bar/".
-   * In most cases please use '/' !!!
-   * Detail: https://cli.vuejs.org/config/#publicpath
-   */
-  publicPath: defaultSettings.publicPath,
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
-  devServer: {
-    port: port,
-    overlay: {
-      warnings: false,
-      errors: true
-    }
-  },
-  configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name,
-    externals: {
-      moment: 'moment'
-    }
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
   }
-}
+};
