0cd1a5fcf527a40bfcd89193bbc3b5bbb7027e4d
Ivan Mota <ivan_mota09@hotmail.com>
2021-02-09 18:26:21 -0600

    projects page

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -101,52 +101,47 @@
             type="primary"
           >
             <tab-pane title="Profile">
-              <i slot="label" class="now-ui-icons design_image"></i>
+              <i slot="label" class="now-ui-icons business_globe"></i>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
                   <div class="col-md-6">
-                    <img src="img/bg6.jpg" class="img-raised" />
-                    <img src="img/bg11.jpg" alt="" class="img-raised" />
+                    <img src="img/log-2.png" alt="" class="img-raised" />
                   </div>
                   <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
+                    <img src="img/log.png" alt="" class="img-raised" />
                   </div>
                 </div>
               </div>
             </tab-pane>
 
             <tab-pane title="Home">
-              <i slot="label" class="now-ui-icons location_world"></i>
+              <i slot="label" class="now-ui-icons design_image"></i>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
                   <div class="col-md-6">
-                    <img src="img/bg1.jpg" alt="" class="img-raised" />
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
+                    <img src="img/log-5.png" alt="" class="img-raised" />
+                    <img src="img/log-3.png" alt="" class="img-raised" />
                   </div>
                   <div class="col-md-6">
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
+                    <img src="img/log-6.png" alt="" class="img-raised" />
+                    <img src="img/log-4.png" alt="" class="img-raised" />
                   </div>
                 </div>
               </div>
             </tab-pane>
 
             <tab-pane title="Messages">
-              <i slot="label" class="now-ui-icons sport_user-run"></i>
+              <i slot="label" class="now-ui-icons tech_controller-modern"></i>
 
               <div class="col-md-10 ml-auto mr-auto">
                 <div class="row collections">
                   <div class="col-md-6">
-                    <img src="img/bg3.jpg" alt="" class="img-raised" />
-                    <img src="img/bg8.jpg" alt="" class="img-raised" />
+                    <img src="img/log-7.png" alt="" class="img-raised" />
                   </div>
-                  <div class="col-md-6">
-                    <img src="img/bg7.jpg" alt="" class="img-raised" />
-                    <img src="img/bg6.jpg" class="img-raised" />
-                  </div>
+                  <!--<div class="col-md-6">
+                  </div>-->
                 </div>
               </div>
             </tab-pane>
